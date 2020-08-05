761e45b471600ab2ad188041f9d9d8f5ec7d579b
Mac <mac@macs-imac.local>
2020-08-05 16:20:31 +0530

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Mac on 05/08/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
